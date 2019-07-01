8cf99e0b333de39c812aac3f82fd7ec371a9c007
rootasjey <jeremiecorpinot@outlook.com>
2019-07-01 02:00:14 +0200

    Optimize layout

--- app/layout.ts
+++ app/layout.ts
@@ -3,96 +3,187 @@ import { me as device } from 'device'
 import { FinalType } from '../common/enumerations'
 import document from 'document';
 
+// Variables declaration
+// ~~~~~~~~~~~~~~~~~~~~~
+
 const screen = device.screen || { width: 348, height: 250 }
 
 const { height, width } = screen
 
 const deviceType = width === 300 && height === 300 ? 'Versa' : 'Ionic'
 
-export const getBatteryPositionY = (chargeLevel: number, charging: boolean) => {
-  const vPos: MultiLayoutNestedNumHash = {
-    Ionic: {
-      default: {
-        circle0: 10,
-        circle1: 25,
-        circle2: 40,
-        circle3: 55,
-      },
-      offset: {
-        circle0: 45,
-        circle1: 60,
-        circle2: 75,
-        circle3: 90,
-      }
+const batteryVPos: MultiLayoutNestedNumHash = {
+  Ionic: {
+    default: {
+      circle0: 10,
+      circle1: 25,
+      circle2: 40,
+      circle3: 55,
     },
-    Versa: {
-      default: {
-        circle0: 10,
-        circle1: 25,
-        circle2: 40,
-        circle3: 55,
-      },
-      offset: {
-        circle0: 45,
-        circle1: 60,
-        circle2: 75,
-        circle3: 90,
-      }
+    offset: {
+      circle0: 45,
+      circle1: 60,
+      circle2: 75,
+      circle3: 90,
+    }
+  },
+  Versa: {
+    default: {
+      circle0: 10,
+      circle1: 25,
+      circle2: 40,
+      circle3: 55,
+    },
+    offset: {
+      circle0: 45,
+      circle1: 60,
+      circle2: 75,
+      circle3: 90,
     }
   }
+}
 
+const deviceBatteryVPos = batteryVPos[deviceType]
+
+const nextAnimationTypes: MultiLayoutNestedFinalTypeHash = {
+  Ionic: {
+    hours: {
+      30: FinalType.hide,
+      150: FinalType.background,
+      270: FinalType.foreground,
+      390: FinalType.background,
+    },
+    minutes: {
+      30: FinalType.hide,
+      150: FinalType.background,
+      270: FinalType.foreground,
+      390: FinalType.background,
+    },
+    seconds: {
+      120: FinalType.hide,
+      150: FinalType.background,
+      180: FinalType.foreground,
+      210: FinalType.background,
+    },
+  },
+  Versa: {
+    hours: {
+      40: FinalType.hide,
+      180: FinalType.background,
+      320: FinalType.foreground,
+      460: FinalType.background,
+    },
+    minutes: {
+      40: FinalType.hide,
+      180: FinalType.background,
+      320: FinalType.foreground,
+      460: FinalType.background,
+    },
+    seconds: {
+      150: FinalType.hide,
+      180: FinalType.background,
+      210: FinalType.foreground,
+      240: FinalType.background,
+    },
+  },
+}
+
+const deviceNextAnimationTypes = nextAnimationTypes[deviceType]
+
+const resetYToCoord: MultiLayoutNumHash = {
+  Ionic: {
+    hours: 390,
+    minutes: 390,
+    seconds: 210,
+  },
+  Versa: {
+    hours: 460,
+    minutes: 460,
+    seconds: 240,
+  },
+}
+
+const deviceResetYToCoord = resetYToCoord[deviceType]
+
+const vN1 = 40 // versa number 1
+const vN2 = 180
+const vN3 = 320
+const vN4 = 460
+
+const nextYPos: MultiLayoutNestedNumHash = {
+  Ionic: {
+    hours: {
+      30: 0,
+      150: 30,
+      270: 150,
+      390: 270,
+    },
+    minutes: {
+      30: 0,
+      150: 30,
+      270: 150,
+      390: 270,
+    },
+    seconds: {
+      120: 90,
+      150: 120,
+      180: 150,
+      210: 180,
+    },
+  },
+  Versa: {
+    hours: {
+      [vN1]: 0,
+      [vN2]: vN1,
+      [vN3]: vN2,
+      [vN4]: vN3,
+    },
+    minutes: {
+      [vN1]: 0,
+      [vN2]: vN1,
+      [vN3]: vN2,
+      [vN4]: vN3,
+    },
+    seconds: {
+      150: 110,
+      180: 150,
+      210: 180,
+      240: 210,
+    },
+  },
+}
+
+const deviceNextYPos = nextYPos[deviceType]
+
+const secondsPositionY: MultiLayoutNumHash = {
+  Ionic: {
+    0: 120,
+    1: 150,
+    2: 180,
+    3: 210,
+  },
+  Versa: {
+    0: 150,
+    1: 180,
+    2: 210,
+    3: 240,
+  }
+}
+
+const deviceSecondsPositionY = secondsPositionY[deviceType]
+
+// Exports
+// ~~~~~~~~~~~~~~~~~~~~~
+
+export const getBatteryPositionY = (chargeLevel: number, charging: boolean) => {
   const vLayout = (chargeLevel < 25 || charging) ? 'offset' : 'default'
-  return  vPos[deviceType][vLayout]
+  return deviceBatteryVPos[vLayout]
 }
 
 /** Returns next animation type according to the current Y coordinate. */
 export const getNextAnimationType = (config: LayoutPositionConfig): FinalType => {
   const { type, y } = config
-
-  const animationTypes: MultiLayoutNestedFinalTypeHash = {
-    Ionic: {
-      hours: {
-        30: FinalType.hide,
-        150: FinalType.background,
-        270: FinalType.foreground,
-        390: FinalType.background,
-      },
-      minutes: {
-        30: FinalType.hide,
-        150: FinalType.background,
-        270: FinalType.foreground,
-        390: FinalType.background,
-      },
-      seconds: {
-        120: FinalType.hide,
-        150: FinalType.background,
-        180: FinalType.foreground,
-        210: FinalType.background,
-      },
-    },
-    Versa: {
-      hours: {
-        40: FinalType.hide,
-        180: FinalType.background,
-        320: FinalType.foreground,
-        460: FinalType.background,
-      },
-      minutes: {
-        40: FinalType.hide,
-        180: FinalType.background,
-        320: FinalType.foreground,
-        460: FinalType.background,
-      },
-      seconds: {
-        150: FinalType.hide,
-        180: FinalType.background,
-        210: FinalType.foreground,
-        240: FinalType.background,
-      },
-    },
-  }
-
-  return animationTypes[deviceType][type][y]
+  return deviceNextAnimationTypes[type][y]
 }
 
 /**
@@ -102,113 +193,15 @@ export const getNextAnimationType = (config: LayoutPositionConfig): FinalType =>
  */
 export const getNextY = (config: LayoutPositionConfig) => {
   const { type, y } = config
-
-  const vN1 = 40 // versa number 1
-  const vN2 = 180
-  const vN3 = 320
-  const vN4 = 460
-
-  const correspondances: MultiLayoutNestedNumHash = {
-    Ionic: {
-      hours: {
-        30: 0,
-        150: 30,
-        270: 150,
-        390: 270,
-      },
-      minutes: {
-        30: 0,
-        150: 30,
-        270: 150,
-        390: 270,
-      },
-      seconds: {
-        120: 90,
-        150: 120,
-        180: 150,
-        210: 180,
-      },
-    },
-    Versa: {
-      hours: {
-        [vN1]: 0,
-        [vN2]: vN1,
-        [vN3]: vN2,
-        [vN4]: vN3,
-      },
-      minutes: {
-        [vN1]: 0,
-        [vN2]: vN1,
-        [vN3]: vN2,
-        [vN4]: vN3,
-      },
-      seconds: {
-        150: 110,
-        180: 150,
-        210: 180,
-        240: 210,
-      },
-    },
-  }
-
-  return correspondances[deviceType][type][y];
+  return deviceNextYPos[type][y];
 }
 
 export const getResetYTo = (type: 'hours' | 'minutes' | 'seconds') => {
-  const yCoord: MultiLayoutNumHash = {
-    Ionic: {
-      hours: 390,
-      minutes: 390,
-      seconds: 210,
-    },
-    Versa: {
-      hours: 460,
-      minutes: 460,
-      seconds: 240,
-    },
-  }
-
-  return yCoord[deviceType][type]
+  return deviceResetYToCoord[type]
 }
-
-export const setScaleClock = (digits: ClockDigits) => {
-  const fontSize = {
-    Ionic: 100,
-    Versa: 80,
-    VersaLite: 80,
-  }
-
-  digits.hours.map((element) => {
-    element.style.fontSize = fontSize[deviceType]
-  })
-
-  digits.minutes.map((element) => {
-    element.style.fontSize = fontSize[deviceType]
-  })
-
-  if (digits.separator) {
-    digits.separator.style.fontSize = fontSize[deviceType]
-  }
-}
-
 
 export const getSecondsPositionY = (index: number) => {
-  const vPos: MultiLayoutNumHash = {
-    Ionic: {
-      0: 120,
-      1: 150,
-      2: 180,
-      3: 210,
-    },
-    Versa: {
-      0: 150,
-      1: 180,
-      2: 210,
-      3: 240,
-    }
-  }
-
-  return vPos[deviceType][index]
+  return deviceSecondsPositionY[index]
 }
 
 export const setActionContainersPosition = () => {
@@ -446,5 +439,25 @@ export const setClockPosition = (digits: ClockDigits) => {
     const textElement = digits.separator as TextElement
     textElement.x = xySeparator[deviceType].x
     textElement.y = xySeparator[deviceType].y
+  }
+}
+
+export const setScaleClock = (digits: ClockDigits) => {
+  const fontSize = {
+    Ionic: 100,
+    Versa: 80,
+    VersaLite: 80,
+  }
+
+  digits.hours.map((element) => {
+    element.style.fontSize = fontSize[deviceType]
+  })
+
+  digits.minutes.map((element) => {
+    element.style.fontSize = fontSize[deviceType]
+  })
+
+  if (digits.separator) {
+    digits.separator.style.fontSize = fontSize[deviceType]
   }
 }
